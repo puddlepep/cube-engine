77b92d200cf2c7cf92b4548d6a10bc78fafada66
puddlepep <huntergreen008@gmail.com>
2021-07-13 15:49:50 -0700

    extended input handler to allow 'just_pressed' and 'just_released' values

--- src/game/input_handler.rs
+++ src/game/input_handler.rs
@@ -3,8 +3,21 @@ use std::collections::HashMap;
 use winit::event::ElementState;
 use winit::event::VirtualKeyCode;
 
+#[derive(Debug, Copy, Clone)]
 pub struct State {
     pub held: bool,
+    pub just_pressed: bool,
+    pub just_released: bool,
+}
+
+impl State {
+    pub fn new() -> State {
+        State {
+            held: false,
+            just_pressed: false,
+            just_released: false,
+        }
+    }
 }
 
 pub struct Mouse {
@@ -28,10 +41,76 @@ impl InputMap {
             mouse: Mouse { 
                 delta: cgmath::Vector2::new(0.0, 0.0),
                 wheel_delta: 0.0,
-                lmb: State { held: false }, 
-                mmb: State { held: false }, 
-                rmb: State { held: false },
+                lmb: State::new(),
+                mmb: State::new(),
+                rmb: State::new(),
             },
+        }
+    }
+
+    pub fn update_key(&mut self, key: &VirtualKeyCode, pressed: bool) {
+
+        match self.keys.get_mut(key) {
+
+            // If the key does exist in the current input map, update it.
+            Some(state) => {
+
+                // Depending on the current and previous state of the key, determine whether or not
+                // the key was 'just_pressed', and update the state accordingly.
+                if pressed {
+                    if state.held == false {
+                        *state = State {
+                            held: true,
+                            just_pressed: true,
+                            just_released: false,
+                        }
+                    }
+                    else {
+                        *state = State {
+                            held: true,
+                            just_pressed: false,
+                            just_released: false,
+                        }
+                    }
+                }
+                else {
+
+                    if state.held == true {
+                        *state = State {
+                            held: false,
+                            just_pressed: false,
+                            just_released: true,
+                        }
+                    }
+                    else {
+                        *state = State {
+                            held: false,
+                            just_pressed: false,
+                            just_released: false,
+                        }
+                    }
+
+                }
+            },
+
+            // If the key does not currently exist in the input map, add it to the list of tracked keys.
+            None => {
+                if pressed {
+                    self.keys.insert(*key, State {
+                        held: true,
+                        just_pressed: true,
+                        just_released: false,
+                    });
+                }
+                else {
+                    self.keys.insert(*key, State {
+                        held: false,
+                        just_pressed: false,
+                        just_released: false,
+                    });
+                }
+            }
+
         }
     }
 
@@ -44,20 +123,8 @@ impl InputMap {
 
                 match input.virtual_keycode {
 
-                    Some(code) => {
-
-                        match self.keys.get_mut(&code) {
-
-                            Some(state) => {
-                                state.held = input.state == ElementState::Pressed;
-                            },
-
-                            None => {
-                                self.keys.insert(code, State { held: input.state == ElementState::Pressed });
-                            }
-
-                        }
-
+                    Some(key) => {
+                        self.update_key(&key, input.state == ElementState::Pressed);
                     },
 
                     None => ()
@@ -68,15 +135,108 @@ impl InputMap {
             winit::event::DeviceEvent::Button { button, state } => {
                 match button {
                     1 => {
-                        self.mouse.lmb.held = state == &ElementState::Pressed;
+                        if state == &ElementState::Pressed {
+                            if self.mouse.lmb.held == false {
+                                self.mouse.lmb = State {
+                                    held: true,
+                                    just_pressed: true,
+                                    just_released: false,
+                                }
+                            }
+                            else {
+                                self.mouse.lmb = State {
+                                    held: true,
+                                    just_pressed: false,
+                                    just_released: false,
+                                }
+                            }
+                        }
+                        else {
+                            if self.mouse.lmb.held == true {
+                                self.mouse.lmb = State {
+                                    held: false,
+                                    just_pressed: false,
+                                    just_released: true,
+                                }
+                            }
+                            else {
+                                self.mouse.lmb = State {
+                                    held: false,
+                                    just_pressed: false,
+                                    just_released: false,
+                                }
+                            }
+                        }
                     },
 
                     2 => {
-                        self.mouse.mmb.held = state == &ElementState::Pressed;
+                        if state == &ElementState::Pressed {
+                            if self.mouse.mmb.held == false {
+                                self.mouse.mmb = State {
+                                    held: true,
+                                    just_pressed: true,
+                                    just_released: false,
+                                }
+                            }
+                            else {
+                                self.mouse.mmb = State {
+                                    held: true,
+                                    just_pressed: false,
+                                    just_released: false,
+                                }
+                            }
+                        }
+                        else {
+                            if self.mouse.mmb.held == true {
+                                self.mouse.mmb = State {
+                                    held: false,
+                                    just_pressed: false,
+                                    just_released: true,
+                                }
+                            }
+                            else {
+                                self.mouse.mmb = State {
+                                    held: false,
+                                    just_pressed: false,
+                                    just_released: false,
+                                }
+                            }
+                        }
                     },
 
                     3 => {
-                        self.mouse.rmb.held = state == &ElementState::Pressed;
+                        if state == &ElementState::Pressed {
+                            if self.mouse.rmb.held == false {
+                                self.mouse.rmb = State {
+                                    held: true,
+                                    just_pressed: true,
+                                    just_released: false,
+                                }
+                            }
+                            else {
+                                self.mouse.rmb = State {
+                                    held: true,
+                                    just_pressed: false,
+                                    just_released: false,
+                                }
+                            }
+                        }
+                        else {
+                            if self.mouse.rmb.held == true {
+                                self.mouse.rmb = State {
+                                    held: false,
+                                    just_pressed: false,
+                                    just_released: true,
+                                }
+                            }
+                            else {
+                                self.mouse.rmb = State {
+                                    held: false,
+                                    just_pressed: false,
+                                    just_released: false,
+                                }
+                            }
+                        }
                     },
 
                     _ => ()
@@ -108,15 +268,21 @@ impl InputMap {
         self.mouse.wheel_delta = 0.0;
     }
 
-    pub fn get_key(&mut self, key: VirtualKeyCode) -> &State {
+    pub fn get_key(&mut self, key: VirtualKeyCode) -> State {
+
+        let state: &State;
 
         if self.keys.get(&key).is_some() { 
-            self.keys.get(&key).unwrap()
+            state = self.keys.get(&key).unwrap();
         }
         else { 
-            self.keys.insert(key, State{ held: false });
-            self.keys.get(&key).unwrap()
+            self.keys.insert(key, State::new());
+            state = self.keys.get(&key).unwrap();
         }
+
+        let result = state.clone();
+        self.update_key(&key, result.held);
+        result
 
     }
 
